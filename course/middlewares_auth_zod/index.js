72081e64af8d83bb97d8d60e013818bafa9c34a2
bhavani shankar <bhavanishankargopu@gmail.com>
2023-12-19 11:05:41 +0530

    added index.js in middlewares

---
+++ course/middlewares_auth_zod/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/health_checkup', (req, res) => {
+    const kidneyId = req.query.id;
+    const userName = req.headers.username;
+    const password = req.headers.password;
+
+    if (userName != "shankar" || password != "pass") {
+        res.status(401).json({ message: "unauthorized" });
+        return;  //early retuns
+    }
+    if (kidneyId != 1 && kidneyId != 2) {
+        res.status(411).json({ message: "Wrong inputs" });
+        return;
+    }
+    res.send("Your heart is healthy");
+});
+
+app.listen(8080, () => {
+    console.log(`Server started at PORT 8080`);
+})
